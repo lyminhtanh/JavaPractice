615339205dbe30ff2e579adb0e15ef9f54cd16db
Unknown <lyminhtanh@gmail.com>
2018-05-19 09:16:59 +0700

    try with multiple resources

--- JavaPractice/src/baeldung/core/TryWithResource.java
+++ JavaPractice/src/baeldung/core/TryWithResource.java
@@ -2,14 +2,19 @@ package baeldung.core;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TryWithResource {
 	public static void main(String[] args) throws FileNotFoundException {
-		try(Scanner scanner = new Scanner(new File("src/resource/text.txt"))){
+		try(Scanner scanner = new Scanner(new File("src/resource/printAndWrite.txt"));
+			PrintWriter writer = new PrintWriter(new File("src/resource/generatedFile.txt"))){
 			while(scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
+				String nextLine = scanner.nextLine();
+				System.out.println(nextLine);
+				writer.write(nextLine);
 			}
 		}
+		
 	}
 }
